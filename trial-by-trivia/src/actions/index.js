7b1eaef47af60d6b971dbc0be47a0d3821f96475
thilak9 <b.thilakshan9@gmail.com>
2022-08-08 18:21:36 +0100

    API fetch is working

--- trial-by-trivia/src/actions/index.js
+++ trial-by-trivia/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export function categoryAction(name) {
     return { 
         type: "CATEGORY",
@@ -18,3 +20,17 @@ export function difficultyAction(diff) {
         payload: diff
     }
 }
+
+export async function fetchQuiz(category, difficulty, number){
+        console.log("INSIDE FETCH FUNCTION")
+        const url = `https://opentdb.com/api.php?amount=${number}&category=${category}&difficulty=${difficulty}&type=multiple`
+        try {
+            let data = await axios.get(url)
+            const quizData = data.data.results
+            return quizData
+           
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
--- trial-by-trivia/src/pages/Game/index.js
+++ trial-by-trivia/src/pages/Game/index.js
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useNavigate} from 'react-router-dom';
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import {fetchQuiz} from '../../actions';
+import  Question  from '../../components/Question';
 
 
 export default function Game() {
 
-    const stateCheck = useSelector((state) => {
-      console.log("STATE IS ",state)
-    })
+    const category = useSelector(state => state.category);
+    const difficulty = useSelector(state => state.difficulty);
+    const number = useSelector(state => state.number);
+
+    const dispatch = useDispatch();
+
+    // useEffect( async () => {
+    //   const results =  await fetchQuiz(category, difficulty, number)
+    // },[])
+
+    let [results, setResults] = useState("")
+    let [currentQuestion, setCurrentQuestion] = useState("")
+
+    // useEffect( () => {
+    //   async function fetchData(category, difficulty, number) {
+    //     const resultsData = await fetchQuiz(category, difficulty, number)
+    //     setResults(resultsData[0])
+    //     setCurrentQuestion(resultsData[0].question)
+    //   }
+    //   fetchData();
+    // }, [])
+  
+  // async function fetchCall(){
+  //   console.log("FETCH")
+  //   const resultsData = await fetchQuiz(category, difficulty, number)
+  //   console.log("FETCH DONE")
+  //   setResults(resultsData[0])
+  //   console.log("STATE CHANGE DONE")
+  // }
+    // const resultsData = await fetchQuiz(category, difficulty, number)
+    //     setResults(resultsData[0])
+
+  useEffect(()=>{
+    dispatch(fetchQuiz(category, difficulty, number))
+    console.log(results)
+  },[])
+
+
 
     let navigate = useNavigate() ;
     const navigateToScore = () => {
       navigate('/score')
     }
+
+
+
+
   return (
     <div>Game
-        <button onClick={navigateToScore}>Finished game</button>
+{/* 
+    <View>
+      <Text>{!data.length ? 'Loading...' : data[0].name}</Text>
+    </View> */}
+
+      {/* <Question currentQuestion={1} results = {results}/> */}
     </div>
   )
 }
