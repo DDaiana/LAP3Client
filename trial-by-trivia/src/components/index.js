fe93377aa4a75da3afe9958df53d46da99796e45
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-10 12:02:49 +0100

    index components exports added

--- trial-by-trivia/src/components/index.js
+++ trial-by-trivia/src/components/index.js
@@ -1,1 +1,5 @@
 export {default as Usercard} from './Usercard';
+export {default as Username} from './Username';
+export {default as Timer} from './Timer';
+export {default as Question} from './Question';
+export {default as PlayerCount} from './PlayerCount';
